c93c9382d0a01a2a03a14d5a0f8d3365ce9e4315
arocnies <arocnies@gmail.com>
2023-03-27 01:11:37 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,4 +4,9 @@ setup(
     name='simtoolkit',
     version='1.0',
     packages=['simtoolkit'],
+    install_requires=[
+        'matplotlib~=3.7.1',
+        'numpy==1.24.2',
+        'numpy-quaternion==2022.4.3'
+    ]
 )
